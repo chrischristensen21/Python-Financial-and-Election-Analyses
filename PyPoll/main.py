b0f4e0762959670a5e695368f22102a04d6b390a
Chris Christensen <chrischristensen21@gmail.com>
2021-07-06 16:37:27 -0700

    pypoll set up

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,3 @@
-
 import os
 
 import csv
@@ -11,4 +10,21 @@ with open(csvpath) as csvfile:
   
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    csv_header = next(csvreader)
+
+    total_votes = 0
+    
+    for row in csvreader:
+        total_votes = total_votes + 1
+
+print(f'            Election Results')
+print(f'-----------------------------------------')
+print(f'Total Number of Votes Cast: {total_votes}')
+print(f'------------------------------------------')
+print(f'Khan:')
+print(f'Correy:')
+print(f'Li:')
+print(f"O'Tooley:")
+print(f'-----------------------------------------')
+print(f'Winner:')
+print(f'-----------------------------------------')
